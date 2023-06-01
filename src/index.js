f4c8d9c95aec9d99f4e0f7ca6ae5b0559d8684f3
LePhuocSang2008 <phuocsang5s147@gmail.com>
2023-06-01 23:10:54 +0700

    register

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./component/login";
+import Register from "./component/sign-up";
+import Home from "./page/home"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route index path="/" element={<App />} />
+      <Route index path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   </BrowserRouter>
 );
